35b2e500ae1f18d757aff551d8855f0526072cae
Shagun Madhikarmi <madhikarma@gmail.com>
2018-11-26 03:16:43 +0000

    Adds blog template for dynamic generation form markdown files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,49 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Link from 'gatsby-link'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      <h1 style={{ display: 'inline-block', borderBottom: '1px solid' }}>
+        Shagun Madhikarmi
+      </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link
+            to={node.fields.slug}
+            css={{ textDecoration: `none`, color: `inherit` }}
+          >
+            <h3 style={{ marginBottom: '4px' }}>
+              {node.frontmatter.title}{' '}
+              <span style={{ color: '#BBB' }}>— {node.frontmatter.date}</span>
+            </h3>
+          </Link>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
